46703a16fc2ca3f654ef9897bd3ab14a54bbe890
Pedro Zier <41027931+pedrozier@users.noreply.github.com>
2022-01-30 11:55:22 -0300

    Add files via upload

---
+++ simple plane controller/Scripts/PlayerControllerX.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerControllerX : MonoBehaviour
+{
+    private float speed = 30f;
+    private float rotationSpeed = 100f;
+    private float horizontalInput;
+    private float propSpeed = 500f;
+    public GameObject prop;
+    
+
+    
+    void Start()
+    {
+
+    }
+
+    
+    void Update()
+    {
+        forward(speed);
+        turn(rotationSpeed);
+        propAnim(propSpeed,prop);
+        
+    }
+
+    private void forward(float s)
+    {
+        transform.Translate(Vector3.forward * s * Time.deltaTime);
+
+    }
+
+    private void turn(float s)
+    {
+        horizontalInput = Input.GetAxis("Horizontal");
+        transform.Rotate(Vector3.right * Time.deltaTime * s * horizontalInput);
+    }
+
+    private void propAnim(float s, GameObject p)
+    {
+        p.transform.Rotate(Vector3.back * s * Time.deltaTime);
+    }
+}
