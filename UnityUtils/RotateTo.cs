e3e6777e43cee605e4fe44856f9538b6547c9723
Pedro Zier <41027931+pedrozier@users.noreply.github.com>
2022-04-18 20:43:31 -0300

    Add files via upload

---
+++ UnityUtils/RotateTo.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RotateTo : MonoBehaviour
+{
+    public GameObject target;
+    
+    void Start()
+    {
+
+    }
+
+    void Update()
+    {
+        RotateToTarget(target);
+    }
+
+    public void RotateToTarget(GameObject target)
+    {
+        Vector3 relativePos = target.transform.position - this.transform.position;
+        this.gameObject.transform.rotation = Quaternion.LookRotation(relativePos, Vector3.up);
+    }
+
+}
